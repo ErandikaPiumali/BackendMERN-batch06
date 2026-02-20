b409970bec45a3a6ece108533e3708ad22d659a7
ErandikaPiumali <epiumali621@gmail.com>
2026-02-20 13:42:37 +0530

    Update project with local version

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,21 @@
 import User from "../model/users.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req,res){
 
-    const user = new User(req.body) // created a user - object
+  //  const user = new User(req.body) // created a user - object
+  const userData={
+    firstName:req.body.firstName,
+    lastName:rea.body.lastName,
+    email:req.body.email,
+    password: passwordHash,
+    phone: req.body.phone,
+
+   
+  }
+ const passwordHash = bcrypt.hashSync(req.body.password,10)
+  const user=new User(userData) // create the object
+
     user.save().then(
         ()=>{
             res.json({
@@ -14,6 +27,39 @@ export function createUser(req,res){
             res.json({
                 message:"Failed to create a user"
             })
+        }
+    )
+
+}
+
+export function loginUser(req,res){
+    const email=req.body.email
+    const password=req.body.password
+
+    User.findOne({
+        email:email
+    }).then(
+        (user)=>{
+            if(user==null){
+                res.status(404).json({
+                    message:"User not found"
+                });
+                return;
+            }else{
+                const isPasswordCorrect = bcrypt.compareSync(password,user.password)
+                if (isPasswordCorrect){
+                    res.json({
+                        message:"Login Successful"
+                    })
+                }else{
+                    res.status(403).json({
+                        message:"Incorrect password"
+                    })
+                }
+            }
+
+            console.log(user)
+            //console.log(User)- this print the whole schema
         }
     )
 
